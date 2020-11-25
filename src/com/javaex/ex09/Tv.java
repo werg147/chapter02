99234f4ac7308df2b466fb66e73c908025299228
werg147 <werg147@naver.com>
2020-11-25 18:23:40 +0900

    객체지향 연습

--- src/com/javaex/ex09/Tv.java
+++ src/com/javaex/ex09/Tv.java
@@ -2,4 +2,52 @@ package com.javaex.ex09;
 
 public class Tv {
 
+	//필드
+	private int channel;
+	private int volume;
+	private boolean power;
+	
+	//생성자
+	public Tv() {}
+	
+	public Tv(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
+	}
+	
+	
+	//메소드(getter,setter 작성하지않음)
+	
+	public void channel(int channel) {
+		
+	}
+	
+	public void volume(int volume) {
+		
+	}
+	
+	public void power(boolean power) {
+		
+	}
+	
+
+	
+	
+	//메소드(일반)
+	public void status() {
+		System.out.println("Tv (채널: " + channel + ",\t볼륨: " + volume + ",\t전원: " + power + ")");
+	}
+	
+	@Override
+	public String toString() {
+		return "Tv [채널: " + channel + ",\t볼륨: " + volume + ",\t전원: " + power + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- src/com/javaex/ex09/TvApp.java
+++ src/com/javaex/ex09/TvApp.java
@@ -3,7 +3,16 @@ package com.javaex.ex09;
 public class TvApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Tv tv = new Tv(7, 20, false);
+		
+		tv.status();
+		System.out.println(tv.toString());
+		
+		tv.power(true);
+		tv.volume(120);
+		tv.status();
+		
 
 	}
 
