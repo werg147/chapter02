02cb4212cf01d4a128cafe09b1e34d86c9aef1ad
werg1 <werg1@DESKTOP-9B10C7H>
2020-11-27 01:51:33 +0900

    Tv 연습문제

--- src/com/javaex/ex09/Tv.java
+++ src/com/javaex/ex09/Tv.java
@@ -1,77 +1,86 @@
 package com.javaex.ex09;
 
 public class Tv {
-
-	//필드
+	
+	//필드 - private제어
 	private int channel;
 	private int volume;
 	private boolean power;
 	
-	//생성자
-	//public Tv() {}
 	
+	//생성자(기본생성자 오버로딩)
 	public Tv(int channel, int volume, boolean power) {
 		this.channel = channel;
 		this.volume = volume;
 		this.power = power;
 	}
 	
-	
-	//메소드(setter 작성하지않음)
+	//메소드 setter제외
+	//getter
 	public int getChannel() {
 		return channel;
 	}
-	
 	public int getVolume() {
 		return volume;
 	}
 	
+	//파워
+	public void power(boolean on) {
+		this.power = on;
+	}
+	
 	//채널
 	public void channel(int channel) {
-		if(channel>=255) {
-			this.channel = 225;
-		} else if(channel<1) {
-			this.channel = 1;
+		if(power==true) {
+			if(channel<0) {
+				this.channel = 0;
+			} else if(channel>255) {
+				this.channel = 255;
+			} else {
+				this.channel = channel;
+			}
 		}
 	}
 	
 	public void channel(boolean up) {
-		
+		if(up==true) {
+			channel(this.channel+1);
+		} else {
+			channel(this.channel-1);
+		}
 	}
 	
 	//볼륨
 	public void volume(int volume) {
-		if(volume<0) {
-			this.volume = 0;
-		} else if(volume>=100) {
-			this.volume = 100;
+		if(power==true) {
+			if(volume<0) {
+				this.volume = 0;
+			} else if(volume>100) {
+				this.volume = 100;
+			} else {
+				this.volume = volume;
+			}
+		}
+	}
+	
+	public void volume(boolean up) {
+		if(up==true) {
+			volume(this.volume+1);
+		} else {
+			volume(this.volume-1);
 		}
 	}
 	
 	
-	//파워
-	public void power(boolean on) {
-		if(on==true) {
-		this.power = on;
-		}
-	}
-	
-
-	
-	
-	//메소드(일반)
+	//일반메소드
 	public void status() {
-		System.out.println("Tv (채널: " + channel + ",\t볼륨: " + volume + ",\t전원: " + power + ")");
+		System.out.println("채널:" + channel + "\t,\t볼륨:" + volume + ",\t전원:" + power);
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Tv [채널: " + channel + ",\t볼륨: " + volume + ",\t전원: " + power + "]";
+		return "Tv [channel=" + channel + ", volume=" + volume + ", power=" + power + "]";
 	}
-	
-	
-	
-	
 	
 	
 	
--- src/com/javaex/ex09/TvApp.java
+++ src/com/javaex/ex09/TvApp.java
@@ -4,38 +4,37 @@ public class TvApp {
 
 	public static void main(String[] args) {
 		
+		//초기값 설정
 		Tv tv = new Tv(7, 20, false);
-		
 		tv.status();
-		System.out.println(tv.toString());
+
+		System.out.println("-----------------------------------------");
 		
-		System.out.println("====================================================");
-		
+		//전원 켜졌을때 볼륨테스트
 		tv.power(true);
 		tv.volume(120);
 		tv.status();
 		
-		System.out.println("====================================================");
+		//볼륨 up이 참이 아니므로 -1
+		tv.volume(false);
+		tv.status();		
 		
-		//tv.volume(false);
-		//tv.status();
+		System.out.println("-----------------------------------------");
 		
+		//채널 테스트
 		tv.channel(0);
 		tv.status();
 		
-		System.out.println("====================================================");
-		
+		//채널 up이 세번 참이므로 +3
 		tv.channel(true);
 		tv.channel(true);
 		tv.channel(true);
 		tv.status();
 		
-		System.out.println("====================================================");
+		System.out.println("-----------------------------------------");
 		
 		tv.power(false);
 		tv.status();
 		
-
 	}
-
 }
